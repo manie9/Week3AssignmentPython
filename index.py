e2b8b422256d003b312c72b9f0c6b57308d5593f
“manie9” <Fiixxssh@gmail.com>
2025-03-24 22:52:52 +0000

    Week3AssignmentPhython

--- index.py
+++ index.py
@@ -6,16 +6,15 @@ def calculate_discount(price, discount_percent):
     else:
         return price
 
-# Prompt the user for input
+
 original_price = float(input("Enter the original price of the item: "))
 discount_percentage = float(input("Enter the discount percentage: "))
 
-# Calculate the final price
+
 final_price = calculate_discount(original_price, discount_percentage)
 
-# Print the result
+
 if final_price == original_price:
     print(f"No discount applied. The original price is: ${final_price:.2f}")
 else:
     print(f"The final price after applying the discount is: ${final_price:.2f}")
-    
